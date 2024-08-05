68da1532198204524d1aacabe177013919a2abf1
Himanshu12866 <manshu010m@gmail.com>
2024-08-05 15:40:17 +0530

    "Updated index.html with minor formatting changes and added script.js with Login component"

---
+++ Day-1/script.js
@@ -0,0 +1,15 @@
+function Login(){
+    return (
+        <div className="card m-2 p-1">
+        <div className="card-header">
+            <h1> <span className="bi bi-user"></span>User Login</h1>
+        </div>
+        <div className="card-body">
+        <label className="form-label">UserName: </label>
+        <input></input>
+
+        </div>
+            
+        </div>
+    )
+}
