82d605bead2429587b8f6513060009b03ee6a721
Himanshu12866 <manshu010m@gmail.com>
2024-08-05 15:45:05 +0530

    "Updated index.html and script.js files: added input fields and button to Login component, and modified script tag in index.html"

--- Day-1/script.js
+++ Day-1/script.js
@@ -5,11 +5,17 @@ function Login(){
             <h1> <span className="bi bi-user"></span>User Login</h1>
         </div>
         <div className="card-body">
-        <label className="form-label">UserName: </label>
-        <input></input>
+        <label className="form-label m-2">UserName: </label>
+        <input className="form-control" type="text" placeholder="Enter Your Name"></input>
+        <label className="form-label m-2">Password :</label>
+        <input className="form-control" type="password" placeholder="Enter Your Passwprd"></input>
 
+        </div>
+        <div className="card-footer">
+            <button className="btn w-100 btn-dark m-2 p-1">Login</button>
         </div>
             
         </div>
     )
 }
+ReactDOM.render(<Login />, document.getElementById("root"))
