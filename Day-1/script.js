a43391c45c157df2c4b9ec0ff5fc4966ce399514
Himanshu12866 <manshu010m@gmail.com>
2024-08-05 15:59:10 +0530

    Modified

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,20 +1,20 @@
-function Login(){
+function Login() {
     return (
         <div className="card m-2 p-1">
-        <div className="card-header">
-            <h1> <span className="bi bi-user"></span>User Login</h1>
-        </div>
-        <div className="card-body">
-        <label className="form-label m-2">UserName: </label>
-        <input className="form-control" type="text" placeholder="Enter Your Name"></input>
-        <label className="form-label m-2">Password :</label>
-        <input className="form-control" type="password" placeholder="Enter Your Passwprd"></input>
+            <div className="card-header text-center">
+                <h1> <span className="bi bi-person-fill"></span>User Login</h1>
+            </div>
+            <div className="card-body">
+                <label className="form-label m-2">UserName: </label>
+                <input className="form-control" type="text" placeholder="Enter Your Name"></input>
+                <label className="form-label m-2">Password :</label>
+                <input className="form-control" type="password" placeholder="Enter Your Passwprd"></input>
 
-        </div>
-        <div className="card-footer">
-            <button className="btn w-100 btn-dark m-2 p-1">Login</button>
-        </div>
-            
+            </div>
+            <div className="card-footer">
+                <button className="btn w-100 btn-dark m-2 p-1">Login</button>
+            </div>
+
         </div>
     )
 }
