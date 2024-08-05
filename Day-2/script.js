47dad1d886fae5e79e06dbcd4bc6ab25e11f340d
Himanshu12866 <manshu010m@gmail.com>
2024-08-05 18:06:10 +0530

    "Updated index.html with new CSS styles and added input group and signin button to NetflixHeader component in script.js"

--- Day-2/script.js
+++ Day-2/script.js
@@ -1,10 +1,22 @@
 
 function NetflixHeader() {
     return (
-       <div className="d-flex justify-content-between align-items-center">
-       <div className="title">NETFLIX</div>
+        <div className="d-flex m-3 ms-5 ps-5 justify-content-between align-items-center">
+            <div className="title">NETFLIX</div>
+            <div className="me-5">
+                <div className="input-group">
+                    <span className="bi bi-translate input-group-text"></span>
+                    <select className="form-control">
+                        <option>English</option>
+                        <option>हिंदी</option>
+                    </select>
+                    <span className="bi bi-caret-down-fill input-group-text"></span>
 
-       </div>
+                    <button className="btn btn-danger mx-1">Signin</button>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
